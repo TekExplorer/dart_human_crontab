5cb993c8814c83cde4a378990b8167e9470061a0
devnote-dev <devnote.dev75@gmail.com>
2023-03-01 00:52:34 +0000

    refactor(test): replace assertions with test matchers

--- test/main_test.dart
+++ test/main_test.dart
@@ -4,68 +4,63 @@ import 'package:test/test.dart';
 void main() {
   group('strict parsing', () {
     test('throws on empty string', () {
-      try {
-        HumanCrontab.parse('');
-        assert(false);
-      } catch (_) {}
+      expect(() => HumanCrontab.parse(''), throwsA(isA<CrontabException>()));
     });
 
     test('throws on invalid format', () {
-      try {
-        HumanCrontab.parse('this is clearly invalid format');
-        assert(false);
-      } catch (_) {}
+      expect(() => HumanCrontab.parse('this is clearly invalid format'),
+          throwsA(isA<CrontabException>()));
     });
 
     test('throws on missing parts', () {
-      try {
-        HumanCrontab.parse('*/5 * * 2');
-        assert(false);
-      } catch (_) {}
+      expect(() => HumanCrontab.parse('*/5 * * 2'),
+          throwsA(isA<CrontabException>()));
     });
 
     test('allows valid crontab format', () {
       final crontab = HumanCrontab.parse('30 4 */1 12 5');
-      assert(crontab.toString() == '30 4 */1 12 5');
-      assert(crontab.toHuman() ==
-          'At 4:30 on every 1st day of the month on Friday in December');
+      expect(crontab.toString(), equals('30 4 */1 12 5'));
+      expect(
+          crontab.toHuman(),
+          equals(
+              'At 4:30 on every 1st day of the month on Friday in December'));
     });
 
     test('allows all wildcards', () {
       final crontab = HumanCrontab.parse('* * * * *');
-      assert(crontab.toString() == '* * * * *');
-      assert(crontab.toHuman() == 'every minute');
+      expect(crontab.toString(), equals('* * * * *'));
+      expect(crontab.toHuman(), equals('every minute'));
     });
   });
 
   group('relaxed parsing', () {
     test('returns null on empty string', () {
-      assert(HumanCrontab.tryParse('') == null);
+      expect(HumanCrontab.tryParse(''), isNull);
     });
 
-    // FIXME: cannot test this as another function handles it
-    //
-    // test('returns null on invalid format', () {
-    //   assert(HumanCrontab.tryParse('this is clearly invalid format') == null);
-    // });
+    test('returns null on invalid format', () {
+      expect(HumanCrontab.tryParse('this is clearly invalid format'), isNull);
+    });
 
     test('returns null on missing parts', () {
-      assert(HumanCrontab.tryParse('*/5 * * 2') == null);
+      expect(HumanCrontab.tryParse('*/5 * * 2'), isNull);
     });
 
     test('allows valid crontab format', () {
       final crontab = HumanCrontab.tryParse('30 4 */1 12 5');
-      assert(crontab != null);
-      assert(crontab.toString() == '30 4 */1 12 5');
-      assert(crontab!.toHuman() ==
-          'At 4:30 on every 1st day of the month on Friday in December');
+      expect(crontab, isNotNull);
+      expect(crontab.toString(), equals('30 4 */1 12 5'));
+      expect(
+          crontab!.toHuman(),
+          equals(
+              'At 4:30 on every 1st day of the month on Friday in December'));
     });
 
     test('allows all wildcards', () {
       final crontab = HumanCrontab.tryParse('* * * * *');
-      assert(crontab != null);
-      assert(crontab.toString() == '* * * * *');
-      assert(crontab!.toHuman() == 'every minute');
+      expect(crontab, isNotNull);
+      expect(crontab.toString(), equals('* * * * *'));
+      expect(crontab!.toHuman(), equals('every minute'));
     });
   });
 }
